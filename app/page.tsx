4ab8dbea2fa9cea970a9855fd8b4ac3643dba645
Prabin <prabinkumar1999@outlook.com>
2024-05-10 06:36:45 +0530

    core updated

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       {/* Header */}
       <Header />
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
       {/* About */}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,58 @@
 import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
 
 type Props = {};
 
 const Hero = (props: Props) => {
-  return <div>Something to say</div>;
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, My Name's Prabin Kumar Pradeep",
+      "Guy-who-loves-coffee.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src={
+          "https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/e76d4296-43f3-493b-9d50-f8e5c142d06c/2117667014/boys-profile-picture-screenshot.png"
+        }
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          {text}
+          <span className="mr-3">
+            <Cursor cursorColor="#F7AB0A" />
+          </span>
+        </h1>
+
+        <div className="pt-5">
+          <Link href={"#about"}>
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href={"#experiences"}>
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href={"#skills"}>
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href={"#projects"}>
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
